a98e0de61cc52e973b9359a5ac8c9a30a5b91fc4
Kat Inskip <kat@inskip.me>
2025-11-11 01:49:30 -0800

    feat: initial commit, i think it works

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Use Joycons as SlimeVR trackers with this middleware application";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    slimevr-wrangler-src = {
+      url = "github:carl-anders/slimevr-wrangler/main";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, slimevr-wrangler-src, ... }@inputs: let
+    inherit (nixpkgs) lib;
+    inherit (lib.attrsets) genAttrs;
+    forAllSystems = genAttrs lib.systems.flakeExposed;
+    pkgs' = system: import nixpkgs {
+      inherit system;
+    };
+  in {
+      devShells = forAllSystems (system: let
+        pkgs = pkgs' system;
+      in {
+        default = import ./shell.nix {inherit pkgs;};
+      });
+
+      packages = forAllSystems (system: let
+        pkgs = pkgs' system;
+        slimevr-wrangler = pkgs.callPackage ./package.nix { inherit slimevr-wrangler-src; };
+      in {
+        inherit slimevr-wrangler;
+        default = slimevr-wrangler;
+      });
+    };
+}
---
+++ package.nix
@@ -0,0 +1,105 @@
+{
+  lib,
+  rustPlatform,
+  atk,
+  cairo,
+  dbus,
+  glib,
+  openssl,
+  pkg-config,
+  treefmt,
+  zlib,
+  systemd,
+  systemdLibs,
+  llvm,
+  wayland,
+  llvmPackages,
+  curl,
+  gcc,
+  xorg,
+  libGL,
+  libxkbcommon,
+  freetype,
+  vulkan-validation-layers,
+  vulkan-headers,
+  vulkan-loader,
+  vulkan-tools,
+  expat,
+  makeBinaryWrapper,
+  slimevr-wrangler-src,
+}:
+rustPlatform.buildRustPackage (_finalAttrs: {
+  pname = "slimevr-wrangler";
+  version = "0.1.0";
+
+  src = slimevr-wrangler-src.outPath;
+
+  buildInputs = [
+    curl
+    gcc
+    openssl
+    pkg-config
+    zlib
+    freetype
+    expat
+    systemdLibs
+    llvmPackages.libclang
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXrandr
+    libGL
+    libxkbcommon
+    freetype
+    freetype.dev
+    vulkan-validation-layers
+    vulkan-headers
+    vulkan-loader
+    vulkan-tools
+    wayland
+  ];
+
+  nativeBuildInputs = [
+    atk
+    cairo
+    dbus
+    dbus.lib
+    glib.out
+    openssl.out
+    pkg-config
+    zlib
+    systemd
+    llvm
+    expat
+    makeBinaryWrapper
+  ];
+
+  runtimeLibs = [
+    wayland
+    vulkan-validation-layers
+    vulkan-loader
+    libxkbcommon
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXrandr
+  ];
+
+  postInstall = ''
+    wrapProgram $out/bin/slimevr-wrangler \
+      --set LD_LIBRARY_PATH "${lib.makeLibraryPath _finalAttrs.runtimeLibs}"
+  '';
+
+  VK_LAYER_PATH = "${vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+  LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+
+  cargoHash = "sha256-dM8yjPEHpsrmleO1pSQk8uGTtuexvBMbOFaI8Nd7KWs=";
+
+  meta = {
+    mainProgram = "slimevr-wrangler";
+    description = "Use Joycons as SlimeVR trackers with this middleware application";
+    homepage = "https://github.com/carl-anders/slimevr-wrangler/";
+    license = [lib.licenses.mit lib.licenses.asl20];
+    maintainers = [];
+  };
+})
